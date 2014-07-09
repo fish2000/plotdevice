259a973397a2319d50d60272545d032ac8fbf18e
Alexander Böhn <fish2000@gmail.com>
2014-07-09 15:38:32 -0700

    Bash scripts to symlink PlotDevice.app/Contents/Frameworks as PlotDevice.app/Contents/Resources/python/plotdevice/Frameworks

--- etc/relpath.sh
+++ etc/relpath.sh
@@ -5,3 +5,34 @@
 #
 #  Created by FI$H 2000 on 7/9/14.
 #
+#  Adapted from source from StackOverflow:
+#  http://stackoverflow.com/a/14914070/298171
+#
+#
+
+relpath () {
+    [ $# -ge 1 ] && [ $# -le 2 ] || return 1
+    current="${2:+"$1"}"
+    target="${2:-"$1"}"
+    [ "$target" != . ] || target=/
+    target="/${target##/}"
+    [ "$current" != . ] || current=/
+    current="${current:="/"}"
+    current="/${current##/}"
+    appendix="${target##/}"
+    relative=''
+    while appendix="${target#"$current"/}"
+        [ "$current" != '/' ] && [ "$appendix" = "$target" ]; do
+        if [ "$current" = "$appendix" ]; then
+            relative="${relative:-.}"
+            echo "${relative#/}"
+            return 0
+        fi
+        current="${current%/*}"
+        relative="$relative${relative:+/}.."
+    done
+    relative="$relative${relative:+${appendix:+/}}${appendix#/}"
+    echo "$relative"
+}
+
+#relpath "$@"
