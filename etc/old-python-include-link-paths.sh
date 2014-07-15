20edea917442b1ceee641aa7e6ba2fa2f7aafd88
Alexander Böhn <fish2000@gmail.com>
2014-07-14 20:46:40 -0700

    Removed old include/link paths

--- etc/old-python-include-link-paths.sh
+++
@@ -1,4 +0,0 @@
-/System/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/config
-$(SYSTEM_LIBRARY_DIR)/Frameworks/Python.framework/Versions/2.7
-
-/System/Library/Frameworks/Python.framework/Versions/2.7/include/python2.7
